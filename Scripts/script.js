4cdfb48247c4e9e612e80c5aeb06a59748faad77
Karlos Daniel <karlos.kssd@gmail.com>
2025-08-27 20:31:42 -0300

    botao

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,3 +2,8 @@ console.log(document.body);
 //selcionar por TAG
 const listaItens = document.getElementsByTagName('li');
 console.log(listaItens);
+
+const botao = document.getElementById('btn');
+botao.addEventListener('click', function(){
+    alert("Você clicou no botão!")
+});
