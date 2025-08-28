13ffa536fc03fac1eba6266f7abb25107f646cad
Karlos Daniel <karlos.kssd@gmail.com>
2025-08-27 21:24:11 -0300

    botao com cor

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,7 +3,11 @@ console.log(document.body);
 const listaItens = document.getElementsByTagName('li');
 console.log(listaItens);
 
-const botao = document.getElementById('btn');
-botao.addEventListener('click', function(){
-    alert("Você clicou no botão!")
+// const botao = document.getElementById('btn');
+// botao.addEventListener('click', function(){
+//     alert("Você clicou no botão!")
+// });
+const cor = document.getElementById('btn');
+cor.addEventListener('click', function(){
+    document.body.style.backgroundColor = '#008000' 
 });
